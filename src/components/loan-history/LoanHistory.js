aef70c07543461ea51065e8e794b37679a916348
Ashish Kumar <83261616+Ashish9140@users.noreply.github.com>
2021-11-16 13:54:24 +0530

    Add files via upload

---
+++ src/components/loan-history/LoanHistory.js
@@ -0,0 +1,53 @@
+import './LoanHistory.css'
+
+export const LoanHistory = () => {
+    return (
+        <div className="loan-history-scrolled">
+            <div className="loan-history">
+                <h2>Loan History</h2>
+                <hr />
+                <table id="customers">
+                    <thead>
+                        <tr>
+                            <th>Apply Date</th>
+                            <th>Laon Amount</th>
+                            <th>Loan Interest</th>
+                            <th>Time Period</th>
+                            <th>Loan Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>22-11-2020</td>
+                            <td>4356345</td>
+                            <td>5%</td>
+                            <td>5 Years</td>
+                            <td>Approved</td>
+                        </tr>
+                        <tr>
+                            <td>22-11-2020</td>
+                            <td>4356345</td>
+                            <td>5%</td>
+                            <td>5 Years</td>
+                            <td>Not Approved</td>
+                        </tr>
+                        <tr>
+                            <td>22-11-2020</td>
+                            <td>4356345</td>
+                            <td>5%</td>
+                            <td>5 Years</td>
+                            <td>Not Approved</td>
+                        </tr>
+                        <tr>
+                            <td>22-11-2020</td>
+                            <td>4356345</td>
+                            <td>5%</td>
+                            <td>5 Years</td>
+                            <td>Approved</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    )
+}
